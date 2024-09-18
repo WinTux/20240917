8b50eff97c6210cb736bb37ced764c3a5667a974
WinTux <rusok.software@gmail.com>
2024-09-18 19:53:48 -0400

    Armando el cliente

---
+++ PrimerEjemplo/ConexionDatos/RestConexionDatos.cs
@@ -0,0 +1,53 @@
+﻿using PrimerEjemplo.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PrimerEjemplo.ConexionDatos
+{
+    public class RestConexionDatos : IRestConexionDatos
+    {
+        public readonly HttpClient httpClient;
+        private readonly string dominio;
+        private readonly string url;
+        private readonly JsonSerializerOptions opcionesJson;
+        public RestConexionDatos()
+        {
+            httpClient = new HttpClient();
+            //dominio = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:7169" : "https://localhost:7169";
+            dominio = "https://localhost:7169";
+            url = $"{dominio}/api";
+            opcionesJson = new JsonSerializerOptions { 
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+        }
+        public Task AddPlatoAsync(Plato plato)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePlatoAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Plato>> GetPlatosAsync()
+        {
+            List<Plato> platos = new List<Plato>();
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            {
+                Debug.WriteLine("[RED] Sin acceso a internet.");
+                return platos;
+            }
+        }
+
+        public Task UpdatePlatoAsync(Plato plato)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PrimerEjemplo/Models/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerEjemplo.Models
+{
+    public class Plato : INotifyPropertyChanged
+    {
+        private int _Id;
+        public int Id { 
+            get { return _Id; }
+            set {
+                if (_Id == value)
+                    return;
+                _Id = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Id)));
+            }
+        }
+        private string _Nombre;
+        public string Nombre {
+            get { return _Nombre; }
+            set {
+                if (_Nombre == value)
+                    return;
+                _Nombre = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Nombre)));
+            } 
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
